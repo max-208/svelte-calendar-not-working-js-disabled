3348a71ae501a25e1f5ea1d72cd92c96fe9df436
Maxime DANIEL <maxime.daniel208@gmail.com>
2023-07-27 16:46:09 +0200

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Calendar from '@event-calendar/core';
+	import DayGrid from '@event-calendar/day-grid';
+
+	let plugins = [DayGrid];
+	let options = {
+		view: 'dayGridMonth',
+		events: [
+			{ title: 'test', start: new Date('2023-07-30T00:00:00'), end: new Date('2023-07-30T09:00:00'), backgroundColor: 'red' },
+			{ title: 'test', start: new Date('2023-07-31T00:00:00'), end: new Date('2023-07-31T09:00:00'), backgroundColor: 'blue' },
+		]
+	};
+	console.log(options.events); 
+	// the events are avaliable server-side even when javascript is disabled client side,
+	// the calendar just doesn't render them for some reason
+</script>
+
+
+<section>
+	<Calendar {plugins} {options} />
+</section>
